0b37a6264856238442127d17f5fb7655f13f0a94
Ricardo Paparoni <paparoniricardo@gmail.com>
2024-10-10 17:21:41 +0200

    done

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 12:02:01 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/10/10 16:58:10 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/10/10 17:21:34 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,10 @@ ft_strlcpy(char *dst, const char *src, int dstsize)
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
-	return (ft_strlen(src));
+	return (i);
 }
 
-
+/*
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -53,4 +52,4 @@ int	main(void)
 	char dst[10];
 	int result = ft_strlcpy(dst, src, 5);
 	ft_putnbr(result);
-}
+}*/
