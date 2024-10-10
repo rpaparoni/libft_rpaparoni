2e5f93ee5eab6421099338c226b9eaa5999c2403
Ricardo Paparoni <paparoniricardo@gmail.com>
2024-10-10 17:49:27 +0200

    test

---
+++ ft_strdup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 17:42:08 by rpaparon          #+#    #+#             */
+/*   Updated: 2024/10/10 17:49:06 by rpaparon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strdup(const char *s)
+{
+	char *str;
+	int i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	str = malloc(i + 1);
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
+
+#include <stdio.h>
+
+int main()
+{
+	char *s = "bon dia";
+	char *str = ft_strdup(s);
+	printf("%s\n", str);
+	free(str);
+	return (0);
+}
