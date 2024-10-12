6058b505418b6f036ec9457962c201e1a10ea97c
Ricardo <paparoniricardo@gmail.com>
2024-10-12 14:34:26 +0200

    test

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,46 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/26 12:00:57 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/10/12 14:29:57 by rpaparon         ###   ########.fr       */
+/*   Created: 2024/10/12 14:33:34 by rpaparon          #+#    #+#             */
+/*   Updated: 2024/10/12 14:34:00 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
-{
-	int	result;
-
-	result = 0;
-	while (*str == ' ')
-	{
-		str++;
-	}
-	if (*str == '-')
-	{
-		str++;
-		while (*str >= '0' && *str <= '9')
-		{
-			result = result * 10 + *str - '0';
-			str++;
-		}
-		return (-result);
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		result = result * 10 + *str - '0';
-		str++;
-	}
-	return (result);
-}
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("%d\n", ft_atoi("      -42"));
-}
