b611ba596a2b2b5dd38c901feb84bdad4c70f0ca
Ricardo Paparoni <paparoniricardo@gmail.com>
2024-10-14 15:34:08 +0200

    done

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putlendl_fd.c                                   :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:15:23 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/10/14 14:25:29 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/10/14 15:33:53 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void ft_putendl_fd(char *s, int fd)
 {
-	
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
+
+/*
+int main()
+{
+	int fd = open("test.txt", O_WRONLY | O_CREAT, 0644);
+
+	ft_putendl_fd("hola nena", fd);
+	close(fd);
+	return (0);
+}*/
