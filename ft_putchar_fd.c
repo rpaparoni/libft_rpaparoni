8d0b6e01f6569e104ad1d665204c1ba297a2dded
Ricardo Paparoni <paparoniricardo@gmail.com>
2024-10-14 15:08:00 +0200

    done

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,13 +6,23 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:15:19 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/10/14 14:23:23 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/10/14 15:07:44 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
- void ft_putchar_fd(char c, int fd)
- {
-	
- }
+void	ft_putchar_fd(char c, int fd)
+{
+	write (fd, &c, 1);
+}
+
+/*
+int main()
+{
+	int fd = open("test.txt", O_WRONLY | O_CREAT, 0644);
+
+	ft_putchar_fd('k', fd);
+	close(fd);
+	return (0);
+}*/
