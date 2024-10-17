fcad9652e03629dc263d306151a56c29128e94b3
Ricardo Paparoni <paparoniricardo@gmail.com>
2024-10-17 17:05:05 +0200

    demo

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,21 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:08:26 by rpaparon          #+#    #+#             */
-/*   Updated: 2024/10/14 14:19:05 by rpaparon         ###   ########.fr       */
+/*   Updated: 2024/10/17 16:58:10 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int counter()
+{
+
+}
+
+static char *duplicate()
+{
+
+}
 
 char **ft_split(char const *s, char c)
 {
